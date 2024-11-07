f1e670beedb86c9640b9a8c0ce0a37738ff39309
JuanDavidDV <juandadazav@gmail.com>
2024-11-06 22:24:15 -0500

    add production environment

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+import { parse } from 'url';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,6 +7,10 @@ dotenv.config();
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
+// Parse JawsDB URL for Heroku
+const dbUrl = process.env.JAWSDB_URL || process.env.LOCAL_DB_URL;
+const dbConfig = dbUrl ? parse(dbUrl) : null;
+
 
 export default {
   development: {
@@ -23,5 +28,23 @@ export default {
     seeds: {
       directory: path.join(dirname, 'seeds'),
     },
-  }
+  },
+  production: {
+    client: 'mysql2',
+    connection: dbConfig
+      ? {
+          host: dbConfig.hostname,
+          port: dbConfig.port || 3306,
+          user: dbConfig.auth.split(':')[0],
+          password: dbConfig.auth.split(':')[1],
+          database: dbConfig.pathname.slice(1),
+        }
+      : null,
+    migrations: {
+      directory: path.join(dirname, 'migrations'),
+    },
+    seeds: {
+      directory: path.join(dirname, 'seeds'),
+    },
+  },
 };
